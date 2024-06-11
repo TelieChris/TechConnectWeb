a14260d4a3e74431033ba161a837c3f3aa777b8e
ndacyayitheoneste <ndacyayitheoneste@gmail.com>
2024-06-11 17:49:42 +0200

    assignment changes

--- backend/assignment form/delete.php
+++ backend/assignment form/delete.php
@@ -1,6 +1,6 @@
 <?php
 require_once("db.php");
-$pdo_statement=$pdo_conn->prepare("delete from courses where id=" . $_GET['id']);
+$pdo_statement=$pdo_conn->prepare("delete from assignments where assignment_id=" . $_GET['assignment_id']);
 $pdo_statement->execute();
 header('location:index.php');
 ?>
