f22a63f0eae897305a2ceac305634a593224c1b3
emerancesijyeniyo80 <emerancesijyeniyo80@gmail.com>
2024-07-10 14:16:09 +0200

    Forum

--- backend/forums/delete.php
+++ backend/forums/delete.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'forum_db');
+$conn = new mysqli('localhost', 'root', '', 'techconnectdb');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
