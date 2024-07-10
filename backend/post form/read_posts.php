adcb836e428178f47afcb788b16fb44771a6478a
sosomutware <sosomutware096@gmail.com>
2024-07-10 12:33:47 +0300

    initial

--- backend/post form/read_posts.php
+++ backend/post form/read_posts.php
@@ -34,6 +34,7 @@ $i11 = $start_from + 1;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posts</title>
     <!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
